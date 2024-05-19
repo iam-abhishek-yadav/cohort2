aa2ea09e2af85ccf5ddca30ee156a62f7f34d1d5
iam-abhishek-yadav <abhrecscs17@gmail.com>
2024-05-19 19:19:39 +0530

    Add: 16_auth/cookies

---
+++ 16_auth/cookies/backend/src/index.ts
@@ -0,0 +1,72 @@
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const JWT_SECRET = "mysupersecret";
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+	cors({
+		credentials: true,
+		origin: "http://localhost:5173",
+	})
+);
+
+interface User {
+	id: number;
+	email: string;
+}
+
+interface AuthRequest extends Request {
+	user?: User;
+}
+
+const getUserByEmailAndPassword = (email: string, password: string) => {
+	if (email === "user@example.com" && password === "password") {
+		return { id: 1, email: "user@example.com" };
+	}
+	return null;
+};
+
+const getUserById = (id: number): User | null => {
+	if (id === 1) {
+		return { id: 1, email: "user@example.com" };
+	}
+	return null;
+};
+
+app.get("/user", (req, res) => {
+	const token = req.cookies.token;
+	if (!token) return res.status(401).send("Access denied. No token provided");
+	try {
+		const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload & {
+			id: number;
+		};
+		const user = getUserById(decoded.id);
+		if (!user) return res.status(404).send("User not found.");
+		res.send({ userId: user.id, email: user.email });
+	} catch (error) {
+		res.status(400).send("Invalid token");
+	}
+});
+
+app.post("/signin", (req, res) => {
+	const { email, password } = req.body;
+	const user = getUserByEmailAndPassword(email, password);
+	if (!user) return res.status(400).send("Invalid email or password.");
+	const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "1h " });
+	res.cookie("token", token, { httpOnly: true, secure: true });
+	res.send("Logged in!");
+});
+
+app.post("/logout", (req, res) => {
+	res.clearCookie("token");
+	res.json({ message: "Logged out!" });
+});
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
---
+++ 16_auth/cookies/frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from "./components/Signup";
+import { Signin } from "./components/Signin";
+import { User } from "./components/User";
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path={"/signup"}
+					element={<Signup />}
+				/>
+				<Route
+					path={"/signin"}
+					element={<Signin />}
+				/>
+				<Route
+					path={"/user"}
+					element={<User />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
